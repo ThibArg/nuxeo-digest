0834483371a89654b424c161d1d63d2552b627b6
Thibaud Arguillere <targuillere@nuxeo.com>
2014-10-12 17:50:14 -0400

    Fix test compile error with Eclipse

--- src/test/java/org/nuxeo/utils/test/GetBlobDigestTest.java
+++ src/test/java/org/nuxeo/utils/test/GetBlobDigestTest.java
@@ -27,9 +27,9 @@ import org.nuxeo.ecm.automation.AutomationService;
 import org.nuxeo.ecm.automation.OperationChain;
 import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.ecm.core.api.impl.blob.StringBlob;
 import org.nuxeo.ecm.core.test.CoreFeature;
-import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
@@ -42,7 +42,7 @@ import com.google.inject.Inject;
  */
 
 @RunWith(FeaturesRunner.class)
-@Features({PlatformFeature.class, CoreFeature.class})
+@Features({ PlatformFeature.class, CoreFeature.class })
 @Deploy({   "org.nuxeo.ecm.automation.core",
             "org.nuxeo.utils.digest"
 	})
